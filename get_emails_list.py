d14a8ffdf2da377c274aa46cabbfee5bb6a6a6ee
ntoulemonde <192305627+ntoulemonde@users.noreply.github.com>
2025-09-22 15:16:07 +0000

    get_emails.py: streamline

--- get_emails_list.py
+++ get_emails_list.py
@@ -10,32 +10,38 @@ import os
 from grist_api import GristDocAPI
 import pandas as pd
 
-# Log in to GRIST API
-SERVER = "https://grist.numerique.gouv.fr/"
-DOC_ID = os.environ['SSPHUB_DIRECTORY_ID']
-os.environ['GRIST_API_KEY'] = os.environ['MY_GRIST_API_KEY']
+def get_api_login():
+    # Log in to GRIST API
+    SERVER = "https://grist.numerique.gouv.fr/"
+    DOC_ID = os.environ['SSPHUB_DIRECTORY_ID']
+    os.environ['GRIST_API_KEY'] = os.environ['MY_GRIST_API_KEY']
 
-# Defining API details connection
-api_directory = GristDocAPI(DOC_ID, server=SERVER)
-
-# fetch all the rows
-directory_df = api_directory.fetch_table('Contact')
-directory_df = pd.DataFrame(directory_df)
-print(directory_df.groupby('Supprimez_mon_compte')['id'].nunique())
-
-# renaming weird column
-directory_df = directory_df.rename(columns={'gristHelper_Display': 'nom_structure'})
-
-# Selecting useful columns
-cols_to_keep = [
-    'id', 'nom', 'prenom', 'email', 'Structure', 'Ajout_date', 'Supprimez_mon_compte',
-    'Nom_domaine', 'Siren_structure', 'nom_structure',
-]
-directory_df = directory_df[cols_to_keep]
+    # Returning API details connection
+    return GristDocAPI(DOC_ID, server=SERVER)
 
 
-# Function to extract all emails that have not asked to be deteled from directory
-def get_emails(my_directory_df):
+def get_directory_as_df():
+    # fetch all the rows
+    api_directory = get_api_login()
+    directory_df = api_directory.fetch_table('Contact')
+    directory_df = pd.DataFrame(directory_df)
+    print(directory_df.groupby('Supprimez_mon_compte')['id'].nunique())
+
+    # renaming weird column
+    directory_df = directory_df.rename(columns={'gristHelper_Display': 'nom_structure'})
+
+    # Selecting useful columns
+    cols_to_keep = [
+        'id', 'nom', 'prenom', 'email', 'Structure', 'Ajout_date', 'Supprimez_mon_compte',
+        'Nom_domaine', 'Siren_structure', 'nom_structure',
+    ]
+
+    return directory_df[cols_to_keep]
+
+
+def get_emails():
+    # Extract all emails that have not asked to be deteled from directory
+    my_directory_df = get_directory_as_df()
     my_directory_df = (my_directory_df.query('Supprimez_mon_compte == False')
                                       .sort_values(['Nom_domaine', 'nom'])
                                       )
@@ -44,5 +50,5 @@ def get_emails(my_directory_df):
     return '; '.join(my_directory_df['email'])
 
 
-print('')
-print(get_emails(directory_df))
+get_emails()
+
