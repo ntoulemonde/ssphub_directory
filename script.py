b3c07f0a946add8d06340979a0a778965017a3a8
nicolastlm <192305627+ntoulemonde@users.noreply.github.com>
2025-10-14 14:55:04 +0000

    update script

--- script.py
+++ script.py
@@ -2,11 +2,12 @@ import ssphub_directory.my_functions  as my_f
 import importlib
 importlib.reload(my_f)  # When functions are updated
 
-my_f.remove_files_dir('ssphub/project/test')
-my_f.remove_files_dir('ssphub_directory/project')
+# To generate email
+# generate_email(20, 'main', 'Infolettre de rentrée', get_emails())
 
-my_f.fill_all_templates_from_grist()
+# To generate template
+# my_f.remove_files_dir('ssphub/project/test')
+# my_f.fill_all_templates_from_grist()
 
-my_f.fill_all_templates_from_grist('ssphub_directory/template.qmd', directory='ssphub/project')
+# my_f.fill_all_templates_from_grist('ssphub_directory/template.qmd', directory='ssphub/project')
 
-my_f.fill_template('ssphub_directory/template.qmd', get_grist_merge_as_df(), directory_output='ssphub')
